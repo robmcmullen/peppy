521ac8368a11b650b340b0c3b9094d1151ac1dce
robmcmullen <robm@users.sourceforge.net>
2007-01-10 16:34:57 +0000

    add class name to debug output
    svn-revision: r67

--- debug.py
+++ debug.py
@@ -9,16 +9,32 @@ def debuglog(file):
     global logfh
     logfh=open(file,"w")
 
-def dprint(str):
-    import inspect
+def dprint(str=''):
     caller=inspect.stack()[1]
-    logfh.write("%s:%d:%s: %s%s" % (os.path.basename(caller[1]),caller[2],caller[3],str,os.linesep))
+    namespace=caller[0].f_locals
+    if 'self' in namespace:
+        cls=namespace['self'].__class__.__name__+'.'
+    else:
+        cls=''
+    logfh.write("%s:%d %s%s: %s%s" % (os.path.basename(caller[1]),caller[2],cls,caller[3],str,os.linesep))
 
 class debugmixin(object):
     debuglevel=0
         
-    def dprint(self,str,level=1):
-        import inspect
+    def dprint(self,str='',level=1):
         if not hasattr(self,'debuglevel') or self.debuglevel>=level:
             caller=inspect.stack()[1]
-            logfh.write("%s:%d %s: %s%s" % (os.path.basename(caller[1]),caller[2],caller[3],str,os.linesep))
+            logfh.write("%s:%d %s.%s: %s%s" % (os.path.basename(caller[1]),caller[2],caller[0].f_locals['self'].__class__.__name__,caller[3],str,os.linesep))
+
+
+if __name__=="__main__":
+    dprint('testing')
+    
+    class test(debugmixin):
+        debuglevel=1
+
+        def method(self):
+            self.dprint('need classname')
+
+    t=test()
+    t.method()
