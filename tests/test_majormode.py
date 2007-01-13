159871d544495763090260074a640041db2493da
robmcmullen <robm@users.sourceforge.net>
2007-01-13 12:51:25 +0000

    Fixes #72: * moved tests out of views.py and into new tests subdir * unit tests are run with 'nosetests -i tests' from the main directory
    svn-revision: r69

---
+++ tests/test_majormode.py
@@ -0,0 +1,65 @@
+import os,sys,re
+
+from views import *
+from plugin import *
+from debug import *
+
+from nose.tools import *
+
+class TestEmacsMode:
+    def setUp(self):
+        comp_mgr=ComponentManager()
+        self.driver=ViewPluginDriver(comp_mgr)
+
+    def testMode(self):
+        test=self.driver.parseEmacs
+        eq_(None,test('#!/nothing/here'))
+        eq_('C++',test('-*-C++-*-')[0])
+        eq_('C++',test('-*- C++ -*-')[0])
+        eq_('C++',test('-*-C++ -*-')[0])
+        eq_('C++',test('-*-  C++-*-')[0])
+        eq_('C++',test('#!/nothing/here -*-C++-*-')[0])
+        eq_('C++',test('#!/nothing/here\n-*-C++-*-')[0])
+        eq_('Python',test('#!/nothing/here\n-*-Python-*-')[0])
+
+    def testModeNoVars(self):
+        test=self.driver.parseEmacs
+        eq_(None,test('#!/nothing/here'))
+        eq_('C++',test('-*-mode:C++-*-')[0])
+        eq_('C++',test('-*- mode:C++-*-')[0])
+        eq_('C++',test('-*- mode:C++  -*-')[0])
+        eq_('C++',test('#!/nothing/here -*-mode:C++-*-')[0])
+        eq_('C++',test('#!/nothing/here\n  -*-   mode:C++  -*-')[0])
+        eq_('Python',test('-*- mode:Python-*-')[0])
+
+    def testModeVars(self):
+        test=self.driver.parseEmacs
+        eq_(None,test('#!/nothing/here'))
+        modestring='-*-mode:C++; var1:val1-*-'
+        eq_('C++',test(modestring)[0])
+        eq_('val1',test(modestring)[1]['var1'])
+        modestring='-*-mode:C++; var1:val1; var2:val2;-*-'
+        eq_('C++',test(modestring)[0])
+        eq_('val1',test(modestring)[1]['var1'])
+        eq_('val2',test(modestring)[1]['var2'])
+        modestring='-*- mode:C++; var1:val1; var2:val2-*-'
+        eq_('C++',test(modestring)[0])
+        eq_('val1',test(modestring)[1]['var1'])
+        eq_('val2',test(modestring)[1]['var2'])
+        modestring='-*- mode:C++ ; var1:val1; var2:val2 -*-'
+        eq_('C++',test(modestring)[0])
+        eq_('val1',test(modestring)[1]['var1'])
+        eq_('val2',test(modestring)[1]['var2'])
+        modestring='#!/nothing/here -*-mode:C++; var1:val1; var2:val2-*-'
+        eq_('C++',test(modestring)[0])
+        eq_('val1',test(modestring)[1]['var1'])
+        eq_('val2',test(modestring)[1]['var2'])
+        modestring='#!/nothing/here -*-   mode:C++  ; var1:val1; var2:val2;  -*-'
+        eq_('C++',test(modestring)[0])
+        eq_('val1',test(modestring)[1]['var1'])
+        eq_('val2',test(modestring)[1]['var2'])
+        modestring='-*- mode:Python; var1:val1; var2:val2; -*-'
+        eq_('Python',test(modestring)[0])
+        eq_('val1',test(modestring)[1]['var1'])
+        eq_('val2',test(modestring)[1]['var2'])
+            
