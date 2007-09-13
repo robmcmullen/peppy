e488cc9948c41c376fe0f896c828ebce795c1fb1
robmcmullen <robm@users.sourceforge.net>
2007-09-13 01:22:04 +0000

    Scroll to the bottom on new input, unless the user is reading something far enough back where the scrollbar is not at the end
    * if always_scroll is true, doesn't matter where the scrollbar is and it will always scroll to the end
    
    svn-revision: r419

--- peppy/plugins/error_log.py
+++ peppy/plugins/error_log.py
@@ -39,6 +39,7 @@ class ErrorLog(Sidebar, PeppySTC, debugmixin):
         'min_width': 100,
         'min_height': 20,
         'show': False,
+        'always_scroll': False,
         }
     
     def __init__(self, parent):
@@ -55,7 +56,24 @@ class ErrorLog(Sidebar, PeppySTC, debugmixin):
         if not paneinfo.IsShown():
             paneinfo.Show(True)
             self.frame._mgr.Update()
+
+        scroll = False
+        if not self.settings.always_scroll:
+            # If we're at the end, scroll down as new lines are added.
+            # If we are scrolled back reading something else, don't
+            # scroll.
+            line = self.GetFirstVisibleLine()
+            visible = self.LinesOnScreen()
+            total = self.GetLineCount()
+            if line >= total - visible:
+                scroll = True
+            else:
+                scroll = False
+            #dprint("top line: %d, visible: %d, total=%d, scroll=%s" % (line, visible, total, scroll))
+            
         self.AddText(message.data)
+        if scroll:
+            self.ScrollToLine(self.GetLineCount())
 
 
 class ErrorLogProvider(Component):
