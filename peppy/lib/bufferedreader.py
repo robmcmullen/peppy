91bf27ecf30362b80f3d73321c9c22916c4948b7
robmcmullen <robm@users.sourceforge.net>
2007-12-07 05:23:32 +0000

    Added WindowReader, a file-like object that wraps another file-like object and narrows it to a subset of the file
    svn-revision: r829

--- peppy/lib/bufferedreader.py
+++ peppy/lib/bufferedreader.py
@@ -97,3 +97,63 @@ class BufferedReader(BufferedReplacementReader):
     def __init__(self,  fh,  num_bytes):
         buffer = fh.read(num_bytes)
         BufferedReplacementReader.__init__(self, fh, buffer)
+
+
+class WindowReader(object):
+    """File-like object wrapper that restricts file read operations to a window
+    region within the file.
+    
+    The window subset is specified by an offset and a length within the
+    containing file.
+    """
+    def __init__(self, fh, offset, length):
+        self.fh = fh
+        self.offset = offset
+        self.len = length
+        self.pos = 0
+        self.debug = False
+
+        # set real file position to start of the window region
+        try:
+            self.fh.seek(self.offset)
+            self.seekable = True
+        except:
+            # It may not throw an IOError, so rather than guessing
+            # what sort of error does show up, just assume that on any
+            # error it's not seekable
+            self.fh.read(self.offset)
+            self.seekable = False
+
+    def seek(self, pos, whence=0):
+        if whence == 1:
+            pos += self.pos
+        elif whence == 2:
+            if self.seekable:
+                self.fh.seek(pos, 2)
+                pos = self.fh.tell() - self.offset
+            else:
+                raise IOError("File not seekable from the end")
+
+        if pos != self.pos:
+            if pos > self.len:
+                pos = self.len
+            if self.seekable:
+                self.pos = pos
+                self.fh.seek(self.offset + self.pos)
+            else:
+                raise IOError("File not seekable.")
+
+    def tell(self):
+        return self.pos
+
+    def read(self, size=None):
+        if size is None:
+            size = self.len - self.pos
+        txt=self.fh.read(size)
+        self.pos+=len(txt)
+        if self.debug: print "read %s from %d (%s)" % (size, self.pos, repr(txt))
+        return txt
+
+    def close(self):
+        self.fh.close()
+        self.fh = None
