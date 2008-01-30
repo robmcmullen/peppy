f8884ec29ef54d29521e5d7e6f9c369bab35fe8a
robmcmullen <robm@users.sourceforge.net>
2008-01-30 18:17:34 +0000

    Added script that helps automatically generate the distribution used to create a py2exe bundle
    svn-revision: r972

---
+++ make-dist.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+make distdir
+
+distdir=`make print-distdir|cut -c11-`
+echo $distdir
+mkdir -p $distdir/peppy/builtins
+mv $distdir/peppy/plugins/* $distdir/peppy/builtins
+mv $distdir/peppy/hsi/hsi_major_mode* $distdir/peppy/builtins
+rmdir $distdir/peppy/plugins
+
+./make-py2exe-plugin-list.py -i $distdir/peppy -d builtins
+
+cat > $distdir/py2exe.sh <<EOF
+#!/bin/bash
+python setup.py py2exe
+
+# The following DLLs are needed for Vista support
+cp c:/python25/lib/site-packages/wx-2.8-msw-unicode/wx/MSVCP71.dll dist
+cp c:/python25/lib/site-packages/wx-2.8-msw-unicode/wx/gdiplus.dll dist
+
+/program-files/NSIS/makensis win-installer.nsi
+EOF
+chmod 755 $distdir/py2exe.sh
+
+# Have to add these packages explicitly here, otherwise it tries to
+# pull packages from site-packages and won't look in the peppy
+# directory even if the packages exist.
+chpat.pl "'peppy.plugins', 'peppy.plugins.games'" "'peppy.builtins', 'peppy.builtins.games'" $distdir/setup.py
+rm $distdir/setup.py.orig
