8d0f0b7ef4894bdfa589e10dd7862b486f5a6718
robmcmullen <robm@users.sourceforge.net>
2008-02-17 23:53:32 +0000

    Added preferences_tab as a keyword to search for i18n strings
    svn-revision: r1059

--- i18n.in/make-pot.sh
+++ i18n.in/make-pot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python pygettext.py -k name -k tooltip -k label -k error -o messages.1 ../peppy
+python pygettext.py -k name -k tooltip -k label -k error -k preferences_tab -k message -o messages.1 ../peppy
 msguniq -s messages.1 -o messages.pot
 
