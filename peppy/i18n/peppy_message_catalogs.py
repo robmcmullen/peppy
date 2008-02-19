900c72accde8f59fcb8618fa88ffea446eef310f
robmcmullen <robm@users.sourceforge.net>
2008-02-19 02:13:44 +0000

    Added Danish message catalog * changed the python message dicts to the new sorted format
    svn-revision: r1074

--- peppy/i18n/peppy_message_catalogs.py
+++ peppy/i18n/peppy_message_catalogs.py
@@ -1,7 +1,8 @@
-supplied_translations = ['en_US', 'ru_RU', 'es_ES', 'de_DE', 'zh_CN', 'zh_TW', 'ja_JP']
+supplied_translations = ['en_US', 'da', 'ru_RU', 'es_ES', 'de_DE', 'zh_CN', 'zh_TW', 'ja_JP']
 
 if False:
     # Dummy imports to trick py2exe into including these
+    import da
     import ru_RU
     import es_ES
     import de_DE
