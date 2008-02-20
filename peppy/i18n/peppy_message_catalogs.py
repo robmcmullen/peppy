3585ac5bf698308dd80f8884706b17d10867a8d0
robmcmullen <robm@users.sourceforge.net>
2008-02-20 05:21:03 +0000

    Added French and Italian initial message catalogs
    svn-revision: r1079

--- peppy/i18n/peppy_message_catalogs.py
+++ peppy/i18n/peppy_message_catalogs.py
@@ -1,9 +1,11 @@
-supplied_translations = ['en_US', 'da', 'ru_RU', 'es_ES', 'de_DE', 'zh_CN', 'zh_TW', 'ja_JP']
+supplied_translations = ['en_US', 'da', 'fr_FR', 'ru_RU', 'it_IT', 'es_ES', 'de_DE', 'zh_CN', 'zh_TW', 'ja_JP']
 
 if False:
     # Dummy imports to trick py2exe into including these
     import da
+    import fr_FR
     import ru_RU
+    import it_IT
     import es_ES
     import de_DE
     import zh_CN
