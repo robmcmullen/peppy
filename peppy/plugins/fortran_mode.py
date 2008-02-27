b26d40bb9ee909cc0cdc35571c91d33653569688
robmcmullen <robm@users.sourceforge.net>
2008-02-27 03:29:42 +0000

    Added Fortran 77 mode
    svn-revision: r1091

---
+++ peppy/plugins/fortran_mode.py
@@ -0,0 +1,41 @@
+# peppy Copyright (c) 2006-2008 Rob McMullen
+# Licenced under the GPLv2; see http://peppy.flipturn.org for more info
+"""Fortran programming language editing support.
+
+Major mode for editing Fortran files.
+"""
+
+import os
+
+import wx
+import wx.stc
+
+from peppy.lib.foldexplorer import *
+from peppy.yapsy.plugins import *
+from peppy.major import *
+from peppy.fundamental import FundamentalMode
+
+class Fortran77Mode(SimpleFoldFunctionMatchMixin, FundamentalMode):
+    """Major mode for editing Fortran 77 (fixed format) files.
+    """
+    keyword = 'Fortran77'
+    editra_synonym = 'Fortran 77'
+    
+    icon = 'icons/page_white_ftn.png'
+    regex = "(\.f|\.for|\.f77|\.f90|\.f95)$"
+    
+    fold_function_match = ["SUBROUTINE ", "FUNCTION ","subroutine ", "function ",
+                           "BLOCK DATA ", "block data "]
+
+    default_classprefs = (
+        IntParam('edge_column', 72),
+        BoolParam('indentation_guides', False),
+       )
+
+
+class FortranModePlugin(IPeppyPlugin):
+    """C plugin to register modes and user interface.
+    """
+   
+    def getMajorModes(self):
+        yield Fortran77Mode
