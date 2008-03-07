edac00a92fd36378742f1f7a09889a54a30d2358
robmcmullen <robm@users.sourceforge.net>
2008-03-07 01:20:05 +0000

    Added simple bookmark list * modified RecentFiles to be more generic * added tabs.context_menu action to put the AddURLBookmark in the tab context menu
    svn-revision: r1138

--- peppy/plugins/openrecent.py
+++ peppy/plugins/openrecent.py
@@ -1,11 +1,11 @@
 # peppy Copyright (c) 2006-2008 Rob McMullen
 # Licenced under the GPLv2; see http://peppy.flipturn.org for more info
-"""
-Recently-loaded files plugin.  This plugin demonstrates many features
-of plugins, including bundling of many interfaces into one class,
-OpenRecentConfExtender.  It adds an item to the menu bar, hooks into
-the buffer load routine, and hooks into the configuration file load
-and save process.
+"""Recently-loaded files plugin.
+
+This plugin demonstrates many features of plugins, including bundling of many
+interfaces into one class, interfacing with the popup menu system, adding menu
+items to the menu bar, how to hook into the buffer load routine, and hooking
+into the configuration file load and save process.
 """
 import os
 
@@ -17,83 +17,163 @@ from peppy.lib.userparams import *
 
 
 class RecentFiles(OnDemandGlobalListAction):
-    name=_("Open Recent")
+    """Open a file from the list of recently opened files.
+    
+    Maintains a list of the most recently opened files so that the next time
+    you start the application you can see what you were editing last time.
+    This list is automatically maintained, so every time you open a new file,
+    it is added to the list.  This list is limited in size by the classpref
+    'list_length' in L{RecentFilesPlugin}.
+    """
+    name = "Open Recent"
     default_menu = ("File", 10)
-    inline=False
+    inline = False
+    add_at_top = True
+    
+    @classmethod
+    def isAcceptableURL(cls, url):
+        # skip files with the about: protocol
+        return url.scheme != 'about' and url.scheme != 'mem'
     
     @classmethod
     def append(cls, msg):
         buffer = msg.data
         url = buffer.url
-        
-        # skip files with the about: protocol
-        if url.scheme == 'about':
-            return
+        cls.appendURL(url)
+    
+    @classmethod
+    def trimList(cls):
+        cls.trimStorage(RecentFilesPlugin.classprefs.list_length)
+    
+    @classmethod
+    def appendURL(cls, url):
+        if cls.isAcceptableURL(url):
+            item = unicode(url)
+            # if we're adding an item that's already in the list, move it
+            # to the top of the list by recreating the list
+            storage = cls.storage
+            if item in storage:
+                newlist = []
+                if cls.add_at_top:
+                    newlist.append(item)
+                for olditem in storage:
+                    if olditem != item:
+                        newlist.append(olditem)
+                if not cls.add_at_top:
+                    newlist.append(item)
+                cls.setStorage(newlist)
+            else:
+                # New items are added at the top of this list
+                if cls.add_at_top:
+                    storage[0:0]=[item]
+                else:
+                    storage.append(item)
+                cls.setStorage(storage)
 
-        item = unicode(url)
-        # if we're adding an item that's already in the list, move it
-        # to the top of the list by recreating the list
-        storage = RecentFiles.storage
-        if item in storage:
-            newlist=[item]
-            for olditem in storage:
-                if olditem != item:
-                    newlist.append(olditem)
-            RecentFiles.setStorage(newlist)
-        else:
-            # New items are added at the top of this list
-            storage[0:0]=[item]
-            RecentFiles.setStorage(storage)
-
-        # Trim list to max number of items
-        RecentFiles.trimStorage(RecentFilesPlugin.classprefs.list_length)
-            
-        cls.calcHash()
+            # Trim list to max number of items
+            cls.trimList()
+                
+            cls.calcHash()
         
     def action(self, index=-1, multiplier=1):
-        assert self.dprint("opening file %s" % (RecentFiles.storage[index]))
-        self.frame.open(RecentFiles.storage[index])
+        assert self.dprint("opening file %s" % (self.storage[index]))
+        self.frame.open(self.storage[index])
 
+
+class URLBookmarks(RecentFiles):
+    """Open the previously saved URL.
+    
+    Bookmarks are saved URLs that operate very much like L{RecentFiles} shown
+    in the Open Recent menu.  The only differences are that the files are
+    added only using the L{AddURLBookmark} action, and the files show up in the
+    list in the order that they were added.
+    
+    This list is not limited in size.
+    """
+    name = "Bookmarks"
+    default_menu = ("File", 11)
+    inline = False
+    add_at_top = False
+    global_id = None
+    
+    @classmethod
+    def isAcceptableURL(cls, url):
+        return True
+
+    @classmethod
+    def trimList(cls):
+        pass
+
+class AddURLBookmark(SelectAction):
+    """Add to the saved URL (i.e. bookmarks) list"""
+    alias = "add-bookmark"
+    name = "Add To Bookmarks"
+    tooltip = "Add this URL to the list of bookmarked URLs"
+
+    def action(self, index=-1, multiplier=1):
+        assert self.dprint("id=%x name=%s index=%s" % (id(self),self.name,str(index)))
+        # If it's called from a popup, use the tab on which it was clicked, not
+        # the current mode
+        try:
+            mode = self.frame.tabs.getContextMenuWrapper().editwin
+        except IndexError:
+            mode = self.mode
+        URLBookmarks.appendURL(mode.buffer.url)
 
 
 class RecentFilesPlugin(IPeppyPlugin):
     default_classprefs = (
-        StrParam('history_file', 'recentfiles.txt', 'Filename used in the peppy configuration directory\nto store the list of recently opened files.'),
-        IntParam('list_length', 10, 'Truncate the list to this number of\nfiles most recently opened.'),
+        StrParam('history_file', 'recentfiles.txt', 'Filename used in the peppy configuration directory to store the list of recently opened files.'),
+        IntParam('list_length', 20, 'Truncate the list to this number of files most recently opened.'),
+        StrParam('bookmarks_file', 'bookmarks.txt', 'Filename used in the peppy configuration directory to store the list of bookmarked files.'),
         )
 
-    def activate(self):
-        IPeppyPlugin.activate(self)
+    def activateHook(self):
         Publisher().subscribe(RecentFiles.append, 'buffer.opened')
+        Publisher().subscribe(self.getTabMenu, 'tabs.context_menu')
 
-    def deactivate(self):
-        IPeppyPlugin.deactivate(self)
+    def deactivateHook(self):
         Publisher().unsubscribe(RecentFiles.append)
-        
+        Publisher().unsubscribe(self.getTabMenu)
+
     @classmethod
-    def getFile(cls):
-        filename=cls.classprefs.history_file
+    def getFile(cls, filename):
         app = wx.GetApp()
         pathname=app.getConfigFilePath(filename)
         return pathname
 
-    def initialActivation(self):
+    @classmethod
+    def loadStorage(cls, actioncls, filename):
         storage=[]
-        pathname = self.getFile()
+        pathname = cls.getFile(filename)
         try:
             fh=open(pathname)
             for line in fh:
                 storage.append(line.decode('utf8').rstrip())
         except:
             pass
-        RecentFiles.setStorage(storage)
+        actioncls.setStorage(storage)
 
-    def requestedShutdown(self):
-        pathname = self.getFile()
+    def initialActivation(self):
+        self.loadStorage(RecentFiles, self.classprefs.history_file)
+        self.loadStorage(URLBookmarks, self.classprefs.bookmarks_file)
+    
+    @classmethod
+    def saveStorage(cls, actioncls, filename):
+        pathname = cls.getFile(filename)
         fh=open(pathname,'w')
-        for file in RecentFiles.storage:
+        for file in actioncls.storage:
             #print "saving %s to %s" % (file,pathname)
             fh.write("%s%s" % (file.encode('utf8'),os.linesep))
 
+    def requestedShutdown(self):
+        self.saveStorage(RecentFiles, self.classprefs.history_file)
+        self.saveStorage(URLBookmarks, self.classprefs.bookmarks_file)
+
+    def getTabMenu(self, msg):
+        action_classes = msg.data
+        action_classes.extend([AddURLBookmark])
+        dprint(action_classes)
+
     def getActions(self):
-        yield RecentFiles
+        return [RecentFiles, URLBookmarks]
