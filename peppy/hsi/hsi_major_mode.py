60ba5c696b150a01260d5dd0ca95961bee95e188
robmcmullen <robm@users.sourceforge.net>
2008-08-02 17:00:22 +0000

    Added data type and endian info to HSI welcome message
    svn-revision: r1555

--- peppy/hsi/hsi_major_mode.py
+++ peppy/hsi/hsi_major_mode.py
@@ -1084,7 +1084,11 @@ class HSIMode(BitmapScroller, MajorMode):
         self.idle_update_menu = True
 
     def getWelcomeMessage(self):
-        return "%dx%dx%d %s image using %s loader" % (self.cube.samples, self.cube.lines, self.cube.bands, self.cube.interleave.upper(), self.dataset.getHandler().format_id)
+        if self.cube.byte_order == HSI.BigEndian:
+            endian = "big endian"
+        else:
+            endian = "little endian"
+        return "%dx%dx%d %s %s %s image using %s loader" % (self.cube.samples, self.cube.lines, self.cube.bands, self.cube.data_type.__name__, endian, self.cube.interleave.upper(), self.dataset.getHandler().format_id)
 
     def setCube(self, index=0):
         self.dataset_index = index
