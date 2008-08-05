1614e9ab22e8e004b16fb1d6f96148ef6d128fe6
robmcmullen <robm@users.sourceforge.net>
2008-08-05 03:31:26 +0000

    Added Czech, Greek, Dutch, Occitan, Polish, Portuguese, Brazillian Portuguese, and Turkish support from Launchpad translations
    svn-revision: r1560

---
+++ peppy/i18n/hu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#This is generated code - do not edit
+encoding = 'utf-8'
+dict = {
+'&About...': '&N\xc3\xa9vjegy',
+'&Help': '&Seg\xc3\xadts\xc3\xa9g',
+'&Help...': '&Seg\xc3\xadts\xc3\xa9g...',
+'&New Window': '\xc3\x9aj &ablak',
+'&Revert': '&Alap\xc3\xa1llapot',
+'&Save...': 'M&ent\xc3\xa9s...',
+'&Word Count': 'S&z\xc3\xb3sz\xc3\xa1m',
+'About this program': 'A programr\xc3\xb3l',
+}
--- peppy/i18n/peppy_message_catalogs.py
+++ peppy/i18n/peppy_message_catalogs.py
@@ -1,4 +1,4 @@
-supplied_translations = ['en_US', 'ca', 'da', 'de', 'en_GB', 'eo', 'es', 'fr', 'ia', 'it', 'ja', 'ru', 'sv', 'zh_CN', 'zh_TW', 'hu', 'uk']
+supplied_translations = ['en_US', 'ca', 'da', 'de', 'en_GB', 'eo', 'es', 'fr', 'ia', 'it', 'ja', 'ru', 'sv', 'zh_CN', 'zh_TW', 'hu', 'uk', 'cs', 'el', 'nl', 'oc', 'pl', 'pt', 'pt_BR', 'tr']
 
 if False:
     # Dummy imports to trick py2exe into including these
@@ -18,3 +18,11 @@ if False:
     import zh_TW
     import hu
     import uk
+    import cs
+    import el
+    import nl
+    import oc
+    import pl
+    import pt
+    import pt_BR
+    import tr
