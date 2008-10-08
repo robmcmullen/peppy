1bec7e713907d8549c086356cfa0db83e63f8167
robmcmullen <robm@users.sourceforge.net>
2008-10-08 02:59:45 +0000

    Fixed #449: peppy batch file can now properly handle spaces in install path
    svn-revision: r1687

--- peppy.bat
+++ peppy.bat
@@ -5,12 +5,17 @@ rem Don't know how this works, but it does.
 rem
 rem -------------------- DOS section --------------------
 rem You could set PYTHONPATH or TK environment variables here
-python -x %~f0 %*
+python -x "%~f0" %*
 goto exit
- 
+
 """
 # -------------------- Python section --------------------
 import os,sys
+
+# Set default translation as a no-op for the moment (the actual translation is
+# performed on-demand during the GUI creation, not at application start time).
+import __builtin__
+__builtin__._ = unicode
 
 import peppy.main
 
