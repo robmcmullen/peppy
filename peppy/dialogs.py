bcd0073dd93598a4e16fd9e0af8cc010d15a2229
robmcmullen <robm@users.sourceforge.net>
2008-11-11 22:04:44 +0000

    Refactored QuitDialog to call a superclass * added CustomOkDialog
    svn-revision: r1745

--- peppy/dialogs.py
+++ peppy/dialogs.py
@@ -2,23 +2,18 @@
 # Licenced under the GPLv2; see http://peppy.flipturn.org for more info
 import wx
 
-class QuitDialog(wx.Dialog):
+
+class CustomDialog(wx.Dialog):
+    """Base class for dialog box that can change the default button names.
+    
+    Standard message dialogs don't seem to allow changing the button names, and
+    I wanted the buttons to be less ambiguous than a yes/no or ok/cancel.
     """
-    Custom dialog used to confirm application exit.  Standard message
-    dialogs don't seem to allow changing the button names, and I
-    wanted the buttons to be less ambiguous than a yes/no or
-    ok/cancel.
-    """
-    def __init__(self, parent, unsaved):
+    def __init__(self, parent, text=''):
         wx.Dialog.__init__(self, parent, -1, "Unsaved Changes",
                            pos = wx.DefaultPosition,
                            size = wx.DefaultSize,
                            style = wx.DEFAULT_DIALOG_STYLE)
-        
-        discard = wx.Button(self, wx.ID_NO, "Quit and Discard Changes")
-        self.Bind(wx.EVT_BUTTON, self.OnQuit, discard)
-        cancel = wx.Button(self, wx.ID_YES, "Don't Quit")
-        self.Bind(wx.EVT_BUTTON, self.OnCancel, cancel)
         
         txtsizer = wx.BoxSizer(wx.HORIZONTAL)
         if wx.Platform == "__WXGTK__":
@@ -27,15 +22,14 @@ class QuitDialog(wx.Dialog):
             bitmapsize=(32,32)
         bitmap=wx.StaticBitmap(self, -1, wx.ArtProvider.GetBitmap(wx.ART_WARNING, wx.ART_OTHER, bitmapsize))
         txtsizer.Add(bitmap, 0, wx.ALL, 4)
-        stat = wx.StaticText(self, -1, "The following files have unsaved changes:\n\n%s\n\nIf you quit now, these changes will be lost." % "\n".join([buf.displayname for buf in unsaved]))
+        stat = wx.StaticText(self, -1, text)
         txtsizer.Add(stat, 0, wx.ALL, 4)
         
         dlgsizer = wx.BoxSizer(wx.VERTICAL)
         dlgsizer.Add(txtsizer, 0, wx.ALL, 10)
         
         btnsizer = wx.StdDialogButtonSizer()
-        btnsizer.SetNegativeButton(discard)
-        btnsizer.SetAffirmativeButton(cancel)
+        self.setButtons(btnsizer)
         btnsizer.Realize()
         
         dlgsizer.Add(btnsizer, 0, wx.ALL | wx.ALIGN_RIGHT, 10)
@@ -45,8 +39,36 @@ class QuitDialog(wx.Dialog):
         self.Layout()
         self.Fit()
 
-    def OnQuit(self, evt):
+
+class CustomOkDialog(CustomDialog):
+    """Custom dialog used to confirm application exit.
+    """
+    def __init__(self, parent, text, ok_message, cancel_message):
+        self.ok_message = ok_message
+        self.cancel_message = cancel_message
+        CustomDialog.__init__(self, parent, text)
+    
+    def setButtons(self, btnsizer):
+        ok = wx.Button(self, wx.ID_NO, self.ok_message)
+        btnsizer.SetNegativeButton(ok)
+        self.Bind(wx.EVT_BUTTON, self.OnOK, ok)
+        
+        cancel = wx.Button(self, wx.ID_YES, self.cancel_message)
+        btnsizer.SetAffirmativeButton(cancel)
+        self.Bind(wx.EVT_BUTTON, self.OnCancel, cancel)
+    
+    def OnOK(self, evt):
         self.EndModal(wx.ID_OK)
 
     def OnCancel(self, evt):
         self.EndModal(wx.ID_CANCEL)
+
+
+class QuitDialog(CustomOkDialog):
+    """Custom dialog used to confirm application exit.
+    """
+    def __init__(self, parent, unsaved_buffers):
+        text = "The following files have unsaved changes:\n\n%s\n\nIf you quit now, these changes will be lost." % "\n".join([buf.displayname for buf in unsaved_buffers])
+        ok = "Quit and Discard Changes"
+        cancel = "Don't Quit"
+        CustomOkDialog.__init__(self, parent, text, ok, cancel)
