da155d7979c42ccfe9a195a51b450dda028d4824
robmcmullen <robm@users.sourceforge.net>
2009-03-17 18:32:55 +0000

    Fixed old ideom for SelectAction.popup_options
    svn-revision: r1853

--- peppy/plugins/openrecent.py
+++ peppy/plugins/openrecent.py
@@ -243,7 +243,7 @@ class AddToFileCabinet(SelectAction):
         assert self.dprint("id=%x name=%s index=%s" % (id(self),self.name,str(index)))
         # If it's called from a popup, use the tab on which it was clicked, not
         # the current mode
-        if hasattr(self, 'popup_options'):
+        if self.popup_options is not None:
             mode = self.popup_options['mode']
         else:
             mode = self.mode
