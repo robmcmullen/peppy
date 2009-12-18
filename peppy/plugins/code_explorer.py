61cdbbd3d046a258b2f84af8e892f3a5cc51c531
robmcmullen <robm@users.sourceforge.net>
2009-12-18 00:58:47 +0000

    Don't need platform-specific code in treectrl creation now that code explorer is using CustomTreeCtrl
    svn-revision: r2434

--- peppy/plugins/code_explorer.py
+++ peppy/plugins/code_explorer.py
@@ -41,12 +41,8 @@ class CodeExplorerMinorMode(MinorMode, ct.CustomTreeCtrl):
         return hasattr(modecls, 'getFoldHierarchy')
 
     def __init__(self, parent, **kwargs):
-        if wx.Platform == '__WXMSW__':
-            style = wx.TR_HAS_BUTTONS
-            self.has_root = True
-        else:
-            style = wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS|ct.TR_NO_LINES
-            self.has_root = False
+        style = wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS|ct.TR_NO_LINES
+        self.has_root = False
         ct.CustomTreeCtrl.__init__(self, parent, -1, size=(self.classprefs.best_width, self.classprefs.best_height), style=style)
         MinorMode.__init__(self, parent, **kwargs)
         self.root = self.AddRoot(self.mode.getTabName())
